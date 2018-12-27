4c83eddfa66bec6d03d0be5b296a54b2d5b4574a
Timothy Brathwaite <timothyb0912@gmail.com>
2018-12-27 02:07:15 -0800

    Missed changes from initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A repo for holding the data, replication code, and checking code for published discrete choice papers.',
+    author='Timothy Brathwaite',
+    license='MIT',
+)
